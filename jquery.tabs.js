7eb84017a0019f4733a88370fabb6afcf4905370
Kevin Doyle <kdoyle2@gmail.com>
2011-05-24 13:57:59 -0400

    first commit

---
+++ jquery.tabs.js
@@ -0,0 +1,47 @@
+(function($) {  
+	$.fn.tabs = function(options) {
+		var defaults = {
+			tab: "a",
+			section: ".section",
+			initialIndex: 0,
+			current: "current",
+			onComplete: function() {},
+			hideOnClick:false
+		}
+		return this.each(function() {
+			if (options) { 
+				$.extend(defaults, options);
+			}
+			var el, obj, url, current, hash, title;
+			el = $(this);
+			el.delegate(defaults.tab, "click", onClick);
+			function onClick(event){
+				obj = $(event.currentTarget);
+				url = obj.attr("href");
+				current = el.find(defaults.tab+"."+defaults.current);
+				
+				if(!obj.hasClass(defaults.current)){
+					if(url.substring(0,4) !== "http"){
+						current.removeClass(defaults.current);
+						obj.addClass(defaults.current);
+						$(defaults.section).hide();
+						$(url).show();
+						defaults.onComplete.call(this);
+						return false;
+					}
+				} else if(defaults.hideOnClick) {
+					current.removeClass(defaults.current);
+					$(url).hide();
+					return false;
+				} else {
+					return false;	
+				}
+			}
+			if(defaults.initialIndex !== null){
+				el.find(defaults.tab).eq(defaults.initialIndex).trigger("click");
+			} else {
+				$(defaults.section).hide();	
+			}
+		});
+	}
+})(jQuery);
